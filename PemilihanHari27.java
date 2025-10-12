a4107a9b6afe89e8c52be52a5110a0a0247cd57e
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-10-11 18:23:12 -0700

    Modifikasi percobaan 2

--- PemilihanHari27.java
+++ PemilihanHari27.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.sound.sampled.SourceDataLine;
-
 public class PemilihanHari27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
